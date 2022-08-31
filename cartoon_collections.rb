d98e50ece208f9615e6e938aa7960402cb3d5d29
cedricn8 <cedcn-9mail@yahoo.com>
2022-08-30 23:44:05 -0700

    commit tree 44292c60a39229e207fec41314407f65ab93b480

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  index = 0
+  array.each.with_index(1) do |name|
+    index += 1
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |name|
+    "#{name.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect{|item| cheese_types.include?(item)}
 end
